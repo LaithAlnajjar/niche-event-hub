6de6d7ac9b4019bc6e1e5a71ec7225a1e930817e
laithalnajjar <laithalnajjar2004@gmail.com>
2025-11-12 00:24:35 +0300

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/eventhub/NicheEventHubApplication.java
@@ -0,0 +1,13 @@
+package com.eventhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NicheEventHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NicheEventHubApplication.class, args);
+	}
+
+}
