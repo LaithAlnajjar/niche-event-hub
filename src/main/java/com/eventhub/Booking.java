df79ce90aabedf9cbb353b5ad18b79acdc28cc44
laithalnajjar <laithalnajjar2004@gmail.com>
2025-11-21 20:04:30 +0300

    feat: Create booking entity

---
+++ src/main/java/com/eventhub/Booking.java
@@ -0,0 +1,72 @@
+package com.eventhub;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    User user;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id", nullable = false)
+    Event event;
+
+    @Column(nullable = false)
+    LocalDateTime registrationDate;
+
+    @Column(nullable = false)
+    String status;
+
+    public Booking() {
+    }
+
+    public Booking(User user, Event event, LocalDateTime registrationDate, String status) {
+        this.user = user;
+        this.event = event;
+        this.registrationDate = registrationDate;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+
+    public LocalDateTime getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDateTime registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
