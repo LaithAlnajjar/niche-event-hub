14b0ab4da9b5fb910ea9adbf01ae402e2daf932c
laithalnajjar <laithalnajjar2004@gmail.com>
2025-12-09 18:24:27 +0300

    feat: Add custom queries to booking repository

--- src/main/java/com/eventhub/BookingRepository.java
+++ src/main/java/com/eventhub/BookingRepository.java
@@ -1,7 +1,17 @@
 package com.eventhub;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
+
+    @Query("SELECT b.event FROM Booking b WHERE b.user.id = ?1")
+    List<Event> findEventsByUserId(Long userId);
+
+    @Query("SELECT b.user FROM Booking b WHERE b.event.id = ?1")
+    List<User> findUsersByEventId(Long eventId);
+
     boolean existsByUserAndEvent(User user, Event event);
 }
